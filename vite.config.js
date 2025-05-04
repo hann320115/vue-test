308731f5c8142d68986af61ef8fdde99dd671e2b
star <hbill320115@gmail.com>
2025-05-04 15:54:19 +0800

    設定base

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path' // ✅ 加這行
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vue-test/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src') // ✅ 加這段
+      '@': path.resolve(__dirname, 'src') 
     }
   }
 })
